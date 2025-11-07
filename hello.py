bda45486bc9c7cec66428f3a22ca91bc8e4d6e67
miyai-r <misora0205passion@gmail.com>
2025-11-07 11:15:59 +0900

    greet_user関数を追加

--- hello.py
+++ hello.py
@@ -1,10 +1,14 @@
 
+
+
 def main():
     print("Hello, GitHub実務練習！")
+    greet_user("涼子")
+
+def greet_user(name):
+    print(f"こんにちは、{name}さん！ようこそGitHubへ。")
 
 if __name__ == "__main__":
     main()
-
-
 
 
